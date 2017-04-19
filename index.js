ab45f3a01f4c4f6487d08c80ccafb10fde655b59
vkiding <ajiangap@163.com>
2017-04-19 10:21:47 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+require('./src/jdc-button.ju');
+require('./src/jdc-hn.ju');
+require('./src/jdc-list-item.ju');
+require('./src/jdc-panel.ju');
+require('./src/jdc-tip.ju');
+require('./src/jdc-countdown.ju');
+require('./src/jdc-marquee.ju');
+require('./src/jdc-navbar.ju');
+require('./src/jdc-navpage.ju');
+require('./src/jdc-tabbar.ju');
+require('./src/jdc-tabitem.ju');
